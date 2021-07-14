added parser arg for BSE or MSE loss in reconloss, fixed issue in visuals scales, changed normalization back to original

DVAE_WSC_TORCH_THEO/Classes.py
@@ -75,91 +75,101 @@ class DistributedEvalSampler(Sampler):
         """
         self.epoch = epoch
 
-class DVAE(nn.Module):
-    def __init__(self, n_chan,latent_dim):
-        super(DVAE, self).__init__()
+class DAE(nn.Module):
+    def __init__(self, n_chan, bn_dim, skips='000'): #bn_dm = bottleneck dim
+        super(DAE, self).__init__()
         
         
         self.n_chan = n_chan
         self.height = 320
         self.width =  72
         self.h_dim  = 8 
-        self.latent_dim = latent_dim
-        self.enc = nn.Sequential(
+        self.bn_dim = bn_dim
+        self.skips = skips # do we do the skips?
+        alphaELU = 0 #if 0, ELU=ReLU, maybe 0.2?
+        
+        self.conv1 = nn.Sequential(
                                     nn.Conv2d(self.n_chan, self.h_dim*2, 3,2,1),
-                                    nn.LeakyReLU(0.2),
-
-                                    
+                                    nn.BatchNorm2d(self.h_dim*2), # added batchnorm so output is similar to deconv3 input
+                                    nn.ELU(alpha=alphaELU)
+                                    )
+        self.conv2 = nn.Sequential(
                                     nn.Conv2d(self.h_dim*2,self.h_dim*4, 3,2,1),
                                     nn.BatchNorm2d(self.h_dim*4),
-                                    nn.LeakyReLU(0.2),
-
-                                    
+                                    nn.ELU(alpha=alphaELU)
+                                    )
+        self.conv3 = nn.Sequential(
                                     nn.Conv2d(self.h_dim*4,self.h_dim*8,3,2,1),
                                     nn.BatchNorm2d(self.h_dim*8),
-                                    nn.LeakyReLU(0.2)
-
-
+                                    nn.ELU(alpha=alphaELU)
                                     )
+        
         
         indim = self.h_dim*8 * (self.height//8) * (self.width//8)
-        self.z_mean = nn.Linear(indim,self.latent_dim)
-        self.z_log_var = nn.Linear(indim,self.latent_dim)
         
         
-        self.dec1 = nn.Sequential(nn.Linear(self.latent_dim, indim),
+        self.bn = nn.Linear(indim,self.bn_dim)
+        
+        
+        self.dec1 = nn.Sequential(nn.Linear(self.bn_dim, indim),
                                   nn.BatchNorm1d(indim),
-                                  nn.ReLU()
+                                  nn.ELU(alpha=alphaELU)
                                   )
-        self.dec2 = nn.Sequential(nn.ConvTranspose2d(self.h_dim*8, self.h_dim*4, 2, 2),
-                                  nn.BatchNorm2d(self.h_dim*4),
-                                  nn.ReLU(),
-                                  
-                                  nn.ConvTranspose2d(self.h_dim*4, self.h_dim*2 ,2, 2),
-                                  nn.BatchNorm2d(self.h_dim*2),
-                                  nn.ReLU(),
-                                  
-                                  nn.ConvTranspose2d(self.h_dim*2, self.n_chan, 2,2),
-                                  nn.Sigmoid()
+        
+        
+        self.deconv1 = nn.Sequential(   # will be receiving skip connection of conv3
+                                     nn.ConvTranspose2d(self.h_dim*8, self.h_dim*4, 2, 2),
+                                     nn.BatchNorm2d(self.h_dim*4),
+                                     nn.ELU(alpha=alphaELU)
+                                     )
+        self.deconv2 = nn.Sequential( # will be receiving skip connection of conv2
+                                     nn.ConvTranspose2d(self.h_dim*4, self.h_dim*2 ,2, 2),
+                                     nn.BatchNorm2d(self.h_dim*2),
+                                     nn.ELU(alpha=alphaELU)
+                                     )
+        self.deconv3 = nn.Sequential(
+                                    nn.ConvTranspose2d(self.h_dim*2, self.n_chan, 2,2),
+                                    nn.Sigmoid()
                                     )
         
-        # self.t_conv4 = nn.Sequential(
-        #                             nn.ConvTranspose2d(h_dim*2, self.n_chan, 2, stride=2),
-        #                             nn.Sigmoid()
-        #                             )
         
+        self.skip = nn.Identity()
 
 
-    def encode(self, e):
-        e = self.enc(e)
-        
-        input_dim = self.h_dim*8 * self.height//8 * self.width//8
-        
-        h1 = e.view(-1,input_dim)
-        
-        return self.z_mean(h1), self.z_log_var(h1)
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return mu + eps*std
-
-    def decode(self, inp):
-        
-        # input_dim = self.latent_dim*8 * self.height//8 * self.width//8
+    def prep_deconv(self, inp):
+        # inp is output of dec1
         d_input = inp.view(-1,self.h_dim*8, self.height//8 , self.width//8)
-        h3 = self.dec2(d_input)
+        return d_input
 
-        
-        return h3
 
     def forward(self, x):
-        mu, logvar = self.encode(x)
-        z = self.reparameterize(mu, logvar)
-        z = self.dec1(z)
-        z = self.decode(z)
+
+        # encoder
+        out = self.conv1(x)
+        for_skip3 = self.skip(out)
+        out = self.conv2(out)
+        for_skip2 = self.skip(out)
+        out = self.conv3(out)
+        for_skip1 = self.skip(out)
         
-        return z.view(-1,self.n_chan, self.height,self.width), mu, logvar
+        # bottleneck
+        out = self.bn(out)
+        out = self.dec1(out)
+        out = self.prep_deconv(out)
+        
+        #decoder
+        if bool(self.skips[2]) : out = out +  for_skip1     # output of conv3 is added to input of deconv1
+        out = self.deconv1(out)
+        if bool(self.skips[1]) : out = out + for_skip2     # output of conv2 is added to input of deconv2
+        out = self.deconv2(out)
+        if bool(self.skips[0]) : out = out + for_skip3     # output of conv1 is added to input of deconv3
+        out = self.deconv3(out)
+        
+        
+        
+        return out.view(-1,self.n_chan, self.height,self.width)
+
+
 
 
 class DVAE_WSC(nn.Module):
@@ -175,7 +185,7 @@ class DVAE_WSC(nn.Module):
         self.h_dim  = 8 
         self.latent_dim = latent_dim
         self.skips = skips # do we do the skips?
-        alphaELU = 0 #ELU=ReLU, maybe 0.001?
+        alphaELU = 0.2 #if 0, ELU=ReLU, maybe 0.2?
         
         self.conv1 = nn.Sequential(
                                     nn.Conv2d(self.n_chan, self.h_dim*2, 3,2,1),
@@ -409,14 +419,15 @@ class HDF5Dataset(torch.utils.data.Dataset):
         scale = 1e-8
         X = np.nan_to_num(X)
         X = np.clip(X,-1.0*scale,scale)
-        X /= 2*scale
-        X += 0.5 # now X is between 0 and 1
+        X /= scale
+        X += 1 # now X is between 0 and 2
+        # other normalizations: [-1,1] (0cent), [0,1] (05cent), [0,2] (1cent)
         
         # Clip and Scale labels
         label = np.nan_to_num(label)
-        label = np.clip(label,-1.0*scale,scale) #not necessary but we never know...
-        label /= 2*scale
-        label += 0.5 # now label is between 0 and 1
+        label /= scale
+        label += 1 # now label is between 0 and 2
+        # other normalizations: [-1,1] (0cent), [0,1] (05cent), [0,2] (1cent)
 
         # Got to swap axes because pytorch has channel first
         X = np.swapaxes(X,-1,0)

DVAE_WSC_TORCH_THEO/run_train_model_torch.py
@@ -103,6 +103,9 @@ def parse_args():
 
     parser.add_argument('--test_mode', type=str, default='BEST', choices=['BEST', 'LAST'],
                         help='Use BEST (lower validation loss) or LAST model for predictions on test set.')
+    
+    parser.add_argument('--loss', type=str, default='BCE', choices=['BCE', 'MSE'],
+                        help='Use BCE or MSE loss for reconstruction loss in network loss function.')
 
 
     return check_args(parser.parse_args())
@@ -166,10 +169,10 @@ def smoothness(images, h_coeff, v_coeff):       # supposes (N,C,H,W) format for 
     
     return h_loss*h_coeff + v_loss*v_coeff
 
-def loss_function(recon_x, x, mu, logvar,bceCoeff=1, kldCoeff=1, slCoeff=1):
+def loss_function(recon_x, x, mu, logvar,bceCoeff=1, kldCoeff=1, slCoeff=1, recArg = 'BCE'):
     # reconstruction loss:
-    recon_loss  = bceCoeff * F.binary_cross_entropy(recon_x, x, reduction='sum') # /(320*72) for avg over all batch images 
-    
+    if recArg=='BCE': recon_loss  = bceCoeff * F.binary_cross_entropy(recon_x, x, reduction='sum') # /(320*72) for avg over all batch images 
+    if recArg=='MSE': recon_loss  = bceCoeff * torch.mean( torch.pow((recon_x - x),2) ) #recommended bceCoeff=1e3, kldCoeff=1e-3 1e-4
     # smoothness loss:
     smooth_loss = slCoeff * smoothness(recon_x, h_coeff=1, v_coeff=2) # more important that signal be smooth through time (not space)
         
@@ -635,7 +638,8 @@ def main(PROCESS_RANK, WORLD_SIZE, args):
             # Evaluate the loss for this batch
             BCE, KLD, SL = loss_function(recon_batch, data.view(-1,n_comp,t_max, n_stations),
                                          mu, logvar, 
-                                         bceCoeff=alpha, kldCoeff=beta, slCoeff=gamma)
+                                         bceCoeff=alpha, kldCoeff=beta, slCoeff=gamma,
+                                         recArg = args.loss)
 
             loss = BCE + KLD + SL
             
@@ -715,7 +719,8 @@ def main(PROCESS_RANK, WORLD_SIZE, args):
 
                 tBCE, tKLD, tSL = loss_function(recon_batch, data.view(-1,n_comp,t_max, n_stations),
                                                 mu, logvar,
-                                                bceCoeff=alpha, kldCoeff=beta, slCoeff=gamma)
+                                                bceCoeff=alpha, kldCoeff=beta, slCoeff=gamma,
+                                                recArg=args.loss)
 
                 tloss = tBCE + tKLD + tSL
                 

DVAE_WSC_TORCH_THEO/visuals.py
@@ -78,7 +78,7 @@ def plot_loss_vs_epoch(train_loss,val_loss,fig_name='loss_vs_epoch_fig',path=os.
     plot.set_ylabel('Loss (avg over GPUs)')
     up_bound_95 = max(np.quantile(train_loss,q=0.85), 
                       np.quantile(val_loss,q=0.85))
-    low_bound   = 0.85 * min(train_loss.min(), val_loss.min())
+    low_bound   = min(train_loss.min(), val_loss.min())
     plot.set_ylim(bottom=low_bound,
                   top=up_bound_95)
     # plot.set_ylim(top=min(train_loss[erase],val_loss[erase]),
@@ -125,7 +125,7 @@ def plot_multiloss_vs_epoch(train_losses,val_losses, legend_labels,fig_name='los
         #               bottom = 0.9*min(min(train_losses[:,i]),min(val_losses[:,i])))
         up_bound_95 = max(np.quantile(train_losses[:,i],q=0.85), 
                           np.quantile(val_losses[:,i],q=0.85))
-        low_bound   = 0.85 * min(train_losses[:,i].min(), val_losses[:,i].min())
+        low_bound   = min(train_losses[:,i].min(), val_losses[:,i].min())
         plot.set_ylim(bottom=low_bound,
                       top=up_bound_95)
     
@@ -256,7 +256,7 @@ def plot_different_losses(loss_array, fig_name="different_losses_fig", path=os.g
         # axes[col].set_ylim(top=loss_array[col,1][erase],
         #                        bottom = 0.9*min(loss_array[col,1]))
         up_bound_95 = np.quantile(loss_array[col,1],q=0.85)
-        low_bound   = 0.85 * loss_array[col,1].min()
+        low_bound   = loss_array[col,1].min()
         axes[col].set_ylim(bottom=low_bound,
                       top=up_bound_95)
         #if validation loss, plot as well
@@ -265,7 +265,7 @@ def plot_different_losses(loss_array, fig_name="different_losses_fig", path=os.g
                                    c=colors[col], label=loss_array[col,0]+' val')
             up_bound_95 = max(np.quantile(loss_array[col,1],q=0.85), 
                               np.quantile(loss_array[col,2],q=0.85))
-            low_bound   = 0.85 * min(loss_array[col,1].min(), 
+            low_bound   = min(loss_array[col,1].min(), 
                                      loss_array[col,2].min())
             axes[col].set_ylim(bottom=low_bound,
                           top=up_bound_95)